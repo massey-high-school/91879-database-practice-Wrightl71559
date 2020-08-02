2f53d0801d075d633a61f5178e4fc0c29da0283a
Wrightl71559 <60583421+Wrightl71559@users.noreply.github.com>
2020-08-03 09:28:38 +1200

    split code

--- index.php
+++ index.php
@@ -1,48 +1,6 @@
-<!DOCTYPE HTML>
-
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Book Review Database">
-    <meta name="keywords" content="books, reading, fiction, non-fiction, genre, reviews">
-    <meta name="author" content="L Wright">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Book Review Database</title>
-    
-    <!-- Edit the link below / replace with your chosen google font -->
-    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
-    
-    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
-    <link rel="stylesheet" href="css/bookstyle.css"> 
-    
-</head>
-    
-<body>
-    
-    <div class="wrapper">
-    
-
-        
-        <div class="box banner">
-            
-        <!-- logo image linking to home page goes here -->
-        <a href="index.php">
-            <div class="box logo"  title="Logo - Click here to go to the Home Page">
-            <img class="img-circle" src="images/gen_logo.png" width="150" height="150" alt="generic logo" />
-            
-            </div>    <!-- / logo -->
-        </a>
-            
-            <h1>Orchid Reading</h1>
-        </div>    <!-- / banner -->
-
-   
-        
-        <div class = "box side">
-        side bar goes here
-        </div> <!-- / side bar -->
+<?php 
+    include "topbit.php";
+?>
         
         <div class="box main">
             <h2>First Page heading</h2>
@@ -62,10 +20,6 @@
         </div>    <!-- / main -->
         
 
-        <div class="box footer">
-            CC L Wright 2020
-        </div>    <!-- / footer -->
-    
-    </div>  <!-- / wrapper  -->
-    
-</body>        
+<?php 
+    include "bottombit.php";
+?>
